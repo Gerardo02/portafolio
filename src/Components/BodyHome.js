cd18fd9c414627ba83b7ff3c5e0cbe0104bd846f
gerardo jerez <gerardo.jerez@hotmail.com>
2021-02-15 19:13:45 -0700

    final

--- src/Components/BodyHome.js
+++ src/Components/BodyHome.js
@@ -7,55 +7,66 @@ import battleship from '../IMG/battleship.png'
 
 const BodyHome = () => {
     return ( 
-        <div class="justify-center">
-            <div>
-                <img
-                    src={reactimg}
-                    alt="Reactimg"
-                    className="pt-32"
-                ></img>
-                <p className="text-center font-sans font-light text-3xl text-gray-300">Conocimientos de reactJS</p>
-            </div><br />
-            
+        <div>
+            <br /><br /><br /><br /><br /><br /><br /><br />
+            <p className="font-sans font-light text-5xl text-gray-300">Objetivo</p><br />
+            <p className="font-sans font-light text-3xl text-gray-300">Nuestra empresa tiene como objetivo verificar la calidad de sus software mejorandolo y optimisandolo para la mejor seguridad y mejor rendimiento.</p><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+            <p className="font-sans font-light text-5xl text-gray-300">Trabajos realizados y areas de conocimiento:</p><br />
+            <div class="justify-center">
+                <div>
+                    <img
+                        src={reactimg}
+                        alt="Reactimg"
+                        className="pt-32"
+                    ></img>
+                    <p className="text-center font-sans font-light text-3xl text-gray-300">Conocimientos de reactJS</p>
+                </div><br />
+                
 
-            <div>
-                <img
-                    src={bases}
-                    alt="Bases"
-                    className="pt-32"
-                ></img>
-                <p className="text-center font-sans font-light text-3xl text-gray-300">Conocimiento de bases de datos</p>
-            </div><br />
+                <div>
+                    <img
+                        src={bases}
+                        alt="Bases"
+                        className="pt-32"
+                    ></img>
+                    <p className="text-center font-sans font-light text-3xl text-gray-300">Conocimiento de bases de datos</p>
+                </div><br />
 
-            <div>
-                <img
-                    src={opengl}
-                    alt="Opengl"
-                    className="pt-32"
-                ></img>
-                <p className="text-center font-sans font-light text-3xl text-gray-300">Lo basico de OpenGL</p>
+                <div>
+                    <img
+                        src={opengl}
+                        alt="Opengl"
+                        className="pt-32"
+                    ></img>
+                    <p className="text-center font-sans font-light text-3xl text-gray-300">Lo basico de OpenGL</p>
+                </div><br />
+                <div>
+                    <img
+                        src={github}
+                        alt="Github"
+                        className="pt-20"
+                    ></img>
+                    <p className="text-center font-sans font-light text-3xl text-gray-300">Buen manejo de github</p>
+                </div> <br />
+                <div>
+                    <img
+                        src={battleship}
+                        alt="Battleship"
+                        className="pt-20"
+                    ></img>
+                    <p className="text-center font-sans font-light text-3xl text-gray-300">Un battleship escrito en c++ 100% jugable de dos jugadores</p>
+                </div> <br />
+                
+                
             </div><br />
-            <div>
-                <img
-                    src={github}
-                    alt="Github"
-                    className="pt-20"
-                ></img>
-                <p className="text-center font-sans font-light text-3xl text-gray-300">Buen manejo de github</p>
-            </div> <br />
-            <div>
-                <img
-                    src={battleship}
-                    alt="Battleship"
-                    className="pt-20"
-                ></img>
-                <p className="text-center font-sans font-light text-3xl text-gray-300">Un battleship escrito en c++ 100% jugable de dos jugadores</p>
-            </div> <br />
-            
-            <div>6</div><br />
-            <div>7</div><br />
-            <div>8</div><br />
-            <div>9</div><br />
+            <p className="font-sans font-light text-5xl text-gray-300">Contacto</p>
+            <br />
+            <br />
+            <br />
+            <br />
+            <p className="font-sans font-light text-3xl text-gray-300">Correo: approved.by.gerardo@gmail.com</p>
+            <p className="font-sans font-light text-3xl text-gray-300">Telefono: (+52) 644-195-2680</p>
+            <p className="font-sans font-light text-3xl text-gray-300">Facebook: ApprovedByGerardo</p>
         </div>
      );
 }
